ba165f2c3d384868533ff744fc3e5671ae288d69
Breno Salgado <salgado.breno@gmail.com>
2025-02-11 16:57:02 -0300

    added SalesDataParser lib

---
+++ lib/sales_data_parser.rb
@@ -0,0 +1,37 @@
+class SalesDataParser
+  def initialize(file_path)
+    @path = file_path
+  end
+
+  def parse
+    arr = []
+
+    CSV.foreach(@path, col_sep: "\t", headers: true) do |row|
+      purchaser_name = row[0]
+      item_description = row[1]
+      item_price = row[2]
+      purchase_count = row[3]
+      merchant_address = row[4]
+      merchant_name = row[5]
+
+      arr << {
+        client: {
+          name: purchaser_name
+        },
+        merchant: {
+          name: merchant_name,
+          address: merchant_address
+        },
+        item: {
+          description: item_description
+        },
+        sale: {
+          quantity: purchase_count,
+          price: item_price
+        }
+      }
+    end
+
+    arr
+  end
+end
---
+++ test/lib/sales_data_parser_test.rb
@@ -0,0 +1,115 @@
+require "test_helper"
+require 'csv'
+
+class SalesDataParserTest < ActiveSupport::TestCase
+  def parser
+    SalesDataParser.new("test/fixtures/example_input.tab")
+  end
+
+  test "it has a parse method" do
+    assert_respond_to parser, :parse
+  end
+
+  test "it returns an array" do
+    assert_instance_of Array, parser.parse
+  end
+
+  test "it returns an array of hashes" do
+    assert_instance_of Hash, parser.parse.first
+  end
+
+  test "it returns an array of hashes with the correct keys" do
+    assert_equal %i[client merchant item sale], parser.parse.first.keys
+  end
+
+  test "parses" do
+    expected_result = [
+      {
+        client: {
+          name: "João Silva"
+        },
+        merchant: {
+          address: "987 Fake St",
+          name: "Bob's Pizza"
+        },
+        item: {
+          description: "Pepperoni Pizza Slice",
+        },
+        sale: {
+          price: "10.0", #price moved here b/c of "Cute T-Shirt",
+          quantity: "2",
+        }
+      },
+      {
+        client: {
+          name: "Amy Pond"
+        },
+        merchant: {
+          address: "456 Unreal Rd",
+          name: "Tom's Awesome Shop"
+        },
+        item: {
+          description: "Cute T-Shirt",
+        },
+        sale: {
+          quantity: "5",
+          price: "10.0",
+        }
+      },
+      {
+        client: {
+          name: "Marty McFly"
+        },
+        merchant: {
+          address: "123 Fake St",
+          name: "Sneaker Store Emporium"
+        },
+        item: {
+          description: "Cool Sneakers",
+        },
+        sale: {
+          quantity: "1",
+          price: "5.0",
+        }
+      },
+      {
+        client: {
+          name: "Snake Plissken"
+        },
+        merchant: {
+          address: "123 Fake St",
+          name: "Sneaker Store Emporium"
+        },
+        item: {
+          description: "Cool Sneakers",
+        },
+        sale: {
+          quantity: "4",
+          price: "5.0",
+        }
+      },
+      {
+        client: {
+          name: "João Silva"
+        },
+        merchant: {
+          address: "456 Unreal Rd",
+          name: "Tom's Awesome Shop"
+        },
+        item: {
+          description: "Cute T-Shirt",
+        },
+        sale: {
+          quantity: "1",
+          price: "7.95",
+        }
+      }
+    ]
+
+    parser_result = parser.parse
+
+    expected_result.each_with_index { |e,i| 
+      assert_equal parser_result[i], e
+    }
+  end
+end
